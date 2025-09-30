3b7504ad6cd5e678943c4f059a5250d6e84286b6
parthasarathy-testpress <parthasarathy@testpress.in>
2025-09-30 18:39:25 +0530

    feat: Add profile view page

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,13 @@
 from django.contrib.auth import login as auth_login
+from django.contrib.auth.models import User
+
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+
+from django.views.generic import UpdateView
 
 from .forms import SignUpForm
 
@@ -13,3 +21,13 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
+@method_decorator(login_required, name='dispatch')
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ('first_name', 'last_name', 'email', )
+    template_name = 'my_account.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self,queryset=None)-> User:
+        return self.request.user # type: ignore
